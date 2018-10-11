a4c289562f2393dd8b0ae8e4691e141b413ac774
DenilsonDesigns <daniellanguilller@gmail.com>
2018-10-11 23:02:35 +1100

    added billing and clientside build for heroku

--- index.js
+++ index.js
@@ -31,6 +31,18 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+//Behaviour for production env.
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  //like our main.js file, or main.css file
+  app.use(express.static("client/build"));
+  //Express will serve index.html file if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 //////////////////
 ///SERVER SETUP///
 //////////////////
