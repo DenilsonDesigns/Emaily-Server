f5d8a4a47594e2c280bcb42f8a4423c4e6d6f2b4
DenilsonDesigns <daniellanguilller@gmail.com>
2018-10-12 22:50:35 +1100

    survey model and routes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 const bodyParser = require("body-parser");
+require("./models/Survey");
 require("./models/User");
 require("./services/passport");
 const keys = require("./config/keys");
@@ -30,6 +31,7 @@ app.use(passport.session());
 //Require routes
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+require("./routes/surveyRoutes")(app);
 
 //Behaviour for production env.
 if (process.env.NODE_ENV === "production") {
