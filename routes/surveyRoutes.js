6c55881c020b1b8b645fa328eb1710009e2fa7a6
DenilsonDesigns <daniellanguilller@gmail.com>
2018-10-16 12:35:23 +1100

    getting user info from webhooks

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const _ = require("lodash");
+const Path = require("path-parser").default;
+const { URL } = require("url");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
 const Mailer = require("../services/Mailer");
@@ -12,8 +15,16 @@ module.exports = app => {
   });
 
   app.post("/api/surveys/webhooks", (req, res) => {
-    console.log(req.body);
-    res.send({});
+    const events = _.map(req.body, ({ email, url }) => {
+      const pathname = new URL(url).pathname;
+      const p = new Path("/api/surveys/:surveyId/:choice");
+      const match = p.test(pathname);
+      if (match) {
+        return { email, surveyId: match.surveyId, choice: match.choice };
+      }
+    });
+
+    console.log(events);
   });
 
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
